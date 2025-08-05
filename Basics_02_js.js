538b78c319be53245f954e655578106b7c26d717
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-05 08:00:35 +0000

    container config done

---
+++ Basics_02_js.js
@@ -0,0 +1,20 @@
+const account_Id = "Mayank1234"
+let account_Email = "Mayank@332e4.com"
+var account_Password = "123456"
+account_City = "uttar pradesh"
+let account_state;
+
+// account_Id = "123" // not allowed const not change
+account_Email = "hc@hc.com"
+account_Password = "21212121"
+account_City = "bihar"
+
+
+/*
+ |Pefer not to use var
+  because of issue in block scope and functional scope
+
+*/
+
+
+console.log(account_Id, account_Email, account_Password, account_City, account_state);
