7f18bf97119d9f30bd9b86247e9059293d8537ee
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-05 09:32:06 +0000

    container config done

---
+++ Datatypes_js.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all js code as newer version
+
+// alert("hello") // we are using nodejs not browers
+
+console.log("harsh") 
+
+let name = "Mayank"
+let age = "20"
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null);
