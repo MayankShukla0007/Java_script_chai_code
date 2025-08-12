c58ecc0592548d19f96970b3062ac8830c8cb67b
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-12 10:02:00 +0000

    String Feature

---
+++ 11_String_Feature.js
@@ -0,0 +1,8 @@
+const game = new String("Mayank Shukla")
+console.log(game.at(2)); // The zero-based index of the desired code unit. A negative index will count back from the last item.
+// Returns a new String consisting of the single UTF-16 code unit located at the specified index.
+
+console.log(game.charAt(4)); //The zero-based index of the desired character.
+// Returns the character at the specified index.
+
+console.log
