6095c4af8463ac5014aa83cf40073adf197970b9
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-16 10:38:25 +0000

    Function

---
+++ 03_Basics/01_Function.js
@@ -0,0 +1,19 @@
+
+
+ function MYname() {
+    console.log("M");
+console.log("A");
+console.log("Y");
+console.log("A");
+console.log("N");
+console.log("K");
+ }
+
+ // MYname();
+
+ function add2_No(Number1, Number2) {  // () //parameters
+   console.log(Number1 + Number2);
+ }
+
+//  add2_No("Mayank", "Shukla")  // () // Arguments
+const result =  add2_No(3, 5)
