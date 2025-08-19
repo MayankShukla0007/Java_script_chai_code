e068040951a1d6242ece783a3cfd0aeba84d07bc
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-19 09:06:59 +0000

    Function

--- 03_Basics/01_Function.js
+++ 03_Basics/01_Function.js
@@ -11,9 +11,30 @@ console.log("K");
 
  // MYname();
 
- function add2_No(Number1, Number2) {  // () //parameters
-   console.log(Number1 + Number2);
- }
+//  function add2_No(Number1, Number2) {  // () //parameters
+//    console.log(Number1 + Number2);
+//  }
 
 //  add2_No("Mayank", "Shukla")  // () // Arguments
+
+function add2_No(Number1, Number2) {  // () //parameters
+    // let result = Number1 + Number2
+    // return result
+    return Number1 + Number2
+    }
+
 const result =  add2_No(3, 5)
+
+// console.log("result", result);
+
+
+ function LoginUserMesssage(username){
+    //if(username === undefined) {
+    if(!username){ // same to same for if(username === undefined) 
+        console.log("Please enter the username");
+        return
+    }
+     return `${username} just logged in `
+ }
+  console.log(LoginUserMesssage("Mayank Shukla"))
+ console.log(LoginUserMesssage());
