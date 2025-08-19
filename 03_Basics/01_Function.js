8aa9b320eebcf7e8466f9c8e5ce36c53235e2141
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-19 10:16:49 +0000

    Functions

--- 03_Basics/01_Function.js
+++ 03_Basics/01_Function.js
@@ -36,5 +36,34 @@ const result =  add2_No(3, 5)
     }
      return `${username} just logged in `
  }
-  console.log(LoginUserMesssage("Mayank Shukla"))
- console.log(LoginUserMesssage());
+//   console.log(LoginUserMesssage("Mayank Shukla"))
+//  console.log(LoginUserMesssage());
+
+ function calculateCartprice(val1, val2, ...num1){
+     return num1
+ }
+
+// console.log(calculateCartprice(200, 500, 1000, 5000));
+
+ const user = {
+    username: "Mayank Shukla",
+    price: 199
+ }
+
+  function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+  }
+
+  //handleObject(user)
+handleObject({
+    username: "Tony",
+    price: 399
+})
+
+const myNewArray = [200, 300, 500, 7000]
+
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
+
+console.log(returnSecondValue(myNewArray));
