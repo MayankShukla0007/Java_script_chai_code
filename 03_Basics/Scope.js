8da9a2e094562d7bfbaaf510e11460c62d4015e0
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-28 10:38:53 +0000

    Scope

--- 03_Basics/Scope.js
+++ 03_Basics/Scope.js
@@ -7,9 +7,50 @@ let a = 300
    let a = 10
   const b = 20
  
- console.log("INNER: ", a);
+// c onsole.log("INNER: ", a);
  }
   
-  console.log(a);
+//  console.log(a);
 //   console.log(b);
 //  console.log(c)
+
+ function One() {
+    const username = "Mayank Shukla"
+
+    function two() {
+        const website = "Youtube"
+        // console.log(username);
+    }
+   // console.log(website);
+
+  //  two();
+ }
+
+// One()
+
+if(true) {
+    const username = "Mayank Shukla"
+    if(username === "Mayank Shukla") {
+    const website = " Youtube"
+    // console.log(username + website);
+    }
+  //  console.log(website)
+}
+
+ // console.log(username);
+
+
+ // +++++++++++++++++ interesting +++++++++++++++++++++++++++
+
+
+console.log(addone(5));
+ function addone(num) {
+    return num + 1
+ }
+
+
+// addtwo(5)
+ const addtwo = function(num) {
+    return num + 2
+ }
+console.log(addtwo(5));
